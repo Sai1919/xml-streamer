4e1a3b8086ef1f52c70a04a960ba1d3d29a81ea8
Sai1919 <saitejas464@gmail.com>
2016-11-07 14:01:49 +0530

    add test for checking parsing of nodes with text values randomly around nodes

--- test/test.js
+++ test/test.js
@@ -80,5 +80,36 @@ describe('Tests', function () {
       })
       xmlStream.pipe(parser)
     })
+
+    it('should properly parse a xml simple file in which nodes contain text values randomly.', function (done) {
+      var xmlStream = fs.createReadStream('./test/TestFiles/randomText.xml')
+      var parser = new ParserFactory(xmlStream, {resourcePath: '/items/item'})
+      var expectedData = [ { '$': { 'id': '1', 'test': 'hello' }, '_': ' item  one  two',
+                             'subitem': [ { '$': { 'sub': 'TESTING SUB' }, '_': 'one' },
+                                          { '$': { 'sub': '2' }, '_': 'two' } ] },
+                           { '$': { 'id': '2' }, '_': ' item  one two three  four',
+                             'subitem': [ { '_': 'three' }, { '_': 'four' }, { '_': 'five' } ] }
+                         ]
+      var actualData = []
+      var dataEventCount = 0
+
+      parser.on('data', function (data) {
+        actualData.push(data)
+        dataEventCount++
+      })
+
+      parser.on('error', function (err) {
+        done(err)
+      })
+
+      parser.on('end', function () {
+        // console.log('actualData=', JSON.stringify(actualData, null, 1))
+        // console.log('dataEventCount=', dataEventCount)
+        actualData.should.deepEqual(expectedData)
+        dataEventCount.should.equal(2)
+        done()
+      })
+      xmlStream.pipe(parser)
+    })
   })
 })
